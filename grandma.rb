f40954fde6b4d5a56a50e2c73b30ce4d8fbb5c26
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-13 04:44:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (phrase)
-  if phrase == 'I LOVE YOU GRANDMA!'
-    puts "I LOVE YOU TOO PUMPKIN"
-  end
   elsif phrase != phrase.upcase 
     puts "HUH?! SPEAK UP, SONNY!"
+  end 
+ lsifphrase == 'I LOVE YOU GRANDMA!'
+    puts "I LOVE YOU TOO PUMPKIN"
   end
+
   else phrase == phrase.upcase
     puts
     
