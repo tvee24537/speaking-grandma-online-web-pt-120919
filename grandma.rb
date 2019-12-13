f4b620703c1f50f12f7d5b8c06a83e13ef725aa4
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-13 04:47:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma (phrase)
   if phrase != phrase.upcase 
     return "HUH?! SPEAK UP, SONNY!"
   end 
-  elsif phrase == "I LOVE YOU GRANDMA"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
   end
   else phrase == phrase.upcase
